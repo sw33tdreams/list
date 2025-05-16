81138990b69ee50064d0486a829c5e1d72584dee
sw33tdreams <artyom.hkobyanshnik@gmail.com>
2025-05-16 18:48:18 +0400

    invalid

---
+++ list.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  {
+	int val;
+	Node* next;
+}Node;
+
+Node* createNode(int value){
+	Node* p = malloc(sizeof(Node));
+	if(p == NULL){
+		perror("wrong malloc");
+		return p;
+	}
+	p->val = value;
+	p->next = NULL;
+	return p;
+}
+
+void insertAtEnd(Node** head, int value){
+
+if(*head == NULL){
+	*head = createNode(value);
+}
+else{
+	Node* p = *head->next;
+	while(*p != NULL){
+		p = p->next;
+	}
+	p = (Node*)malloc(sizeof(Node));
+}
+}
+
+void insertAtfront(Node** head, int value){
+	Node* temp = createNode(value);
+	temp->next = *head;
+	*head = temp;
+}
+
+void insertAt(Node** head, int index, int value){
+	if(index == 0) insertAtFront(head, value);
+	else{
+		Node* p = *head->next;
+		for(int i = 0; i <= index; i++){
+			p = *p->next;
+
+
+
+
